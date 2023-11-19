4beb2a06084496715cfcf08e7628c3bf46fc1a0b
Sebastián Javier Baetz <s128baetz@gmail.com>
2023-11-19 18:39:57 -0500

    a5: trigger deploy

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const COURSES_URL = "http://localhost:4000/api/courses";
+//const COURSES_URL = "http://localhost:4000/api/courses"; //<- works fine
 const API_BASE = process.env.REACT_APP_API_BASE;
-const MODULES_URL = `${API_BASE}/modules`;
+const COURSES_URL = `${API_BASE}/courses`;
+
 //comment
 export const findModulesForCourse = async (courseId) => {
   const response = await axios.get(`${COURSES_URL}/${courseId}/modules`);
@@ -18,11 +19,11 @@ export const createModule = async (courseId, module) => {
 };
 
 export const deleteModule = async (moduleId) => {
-  const response = await axios.delete(`${MODULES_URL}/${moduleId}`);
+  const response = await axios.delete(`${COURSES_URL}/${moduleId}`);
   return response.data;
 };
 
 export const updateModule = async (module) => {
-  const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
+  const response = await axios.put(`${COURSES_URL}/${module._id}`, module);
   return response.data;
 };
